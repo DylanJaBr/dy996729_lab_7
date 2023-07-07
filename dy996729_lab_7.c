ba6b1ccf95ef54697076dfd11ea2dd50dfa525cb
DylanJaBr <136853920+DylanJaBr@users.noreply.github.com>
2023-07-07 17:04:27 -0400

    Update dy996729_lab_7.c

--- dy996729_lab_7.c
+++ dy996729_lab_7.c
@@ -20,7 +20,7 @@ void bubbleSort(int arr[], int n)
               temp = arr[j];
               arr[j] = arr[j+1];
               arr[j+1] = temp;
-              swaps[j]++;
+              swaps[i]++;
 
            }//end if
 
